f41016ed4138737b3f25f539a1daf11b165fe9ed
cpuskarz <cpuskarz@cisco.com>
2016-07-31 20:44:06 -0500

    just a bit of comment clean up - part 1

--- ahs2.py
+++ ahs2.py
@@ -18,15 +18,14 @@ spark_headers = {'Content-type': 'application/json', 'Authorization': token}
 
 # Spark rooms setup
 def setup_room():
-    print "in setup room function"
+    #print "in setup room function"
     room_items = current_rooms()
-    #print rooms
 
 # Look for a room called "Alert Room"
     alert_room_id = ""
     for room in room_items:
         if room["title"] == "Alert Room":
-            print("Found Room")
+            #print("Found Room")
             alert_room_id = room["id"]
             return alert_room_id
 
@@ -34,7 +33,6 @@ def setup_room():
     if alert_room_id == "":
         alert_room = create_alert_room()
         alert_room_id = alert_room["id"]
-        # pprint(demo_room)
     return alert_room_id
 
 
@@ -42,8 +40,8 @@ def create_alert_room():
     spark_r = spark_host + "/v1/rooms"
     spark_body = {"title":"Alert Room"}
     page = requests.post(spark_r, headers = spark_headers, json=spark_body)
-    print "IN CREATE ALERT ROOM"
-    print page
+    #print "IN CREATE ALERT ROOM"
+    #print page
     room = page.json()
     return room
 
@@ -59,22 +57,22 @@ def current_rooms():
 def add_email_alert_room(email, alert_room_id):
     member = get_membership_for_room(alert_room_id)
     personEmail_id = ""
-    print "MEMBER"
-    print alert_room_id
-    print "X" * 20
+    #print "MEMBER"
+    #print alert_room_id
+    #print "X" * 20
     for k,v in member[0].items():
         if k == 'personEmail':
             personEmail_id = v
-    print personEmail_id
+    #print personEmail_id
 
     if personEmail_id != email:
-        print "in add email alert room"
+        #print "in add email alert room"
         spark_r = spark_host + "/v1/memberships"
         spark_body = {"personEmail": email, "roomId" : alert_room_id}
         page = requests.post(spark_r, headers = spark_headers, json=spark_body)
         membership = page.json()
         return membership
-    else : return
+    else: return
 
 
 
@@ -82,8 +80,7 @@ def get_membership_for_room(alert_room_id):
     spark_r = spark_host + "/v1/memberships?roomId=%s" % (alert_room_id)
     page = requests.get(spark_r, headers = spark_headers)
     memberships = page.json()["items"]
-    print "IN GET MEMBER FOR ROOM"
-    #print memberships
+    #print "IN GET MEMBER FOR ROOM"
     return memberships
 
 
@@ -104,32 +101,10 @@ def main():
         print('%% Could not login to APIC')
         sys.exit(0)
 
-    seltenant = ""
     lowtenant = ""
     tenants = aci.Tenant.get(session)
-    timeo = 0
     message = ""
 
-# orginal block of ahs1.py
-#    while timeo < 3:
-#        for t in tenants:
-#            if t.name == dt:
-#                seltenant = t.name
-#                timeo += 1
-#               print seltenant
-#
-#            if seltenant == t.name:
-#                u = session.get("/api/node/mo/uni/tn-"+ dt +"/health.json")
-#                page = json.loads(u.content)
-#                healthscore =  page["imdata"][0]["healthInst"]['attributes']['twScore']
-#                message = "TENANT %s's  Score: %s" % (seltenant, healthscore)
-#                headers = {'Content-type': 'application/json', 'Authorization': token}
-#                data = {'roomId': roomId, 'text': message}
-#                requests.post(spark_api, headers=headers, data=json.dumps(data))
-#                time.sleep(5)
-#        if seltenant != dt:
-#            print "OUCH"
-#            sys.exit(0)
 
 
     for tt in tenants:
@@ -143,9 +118,6 @@ def main():
             healthscore =  page["imdata"][0]["healthInst"]['attributes']['twScore']
             message = "TENANT %s's  Score: %s" % (lowtenant, healthscore)
             print message
-            #headers = {'Content-type': 'application/json', 'Authorization': token}
-            #data = {'roomId': roomId, 'text': message}
-            #requests.post(spark_api, headers=headers, data=json.dumps(data))
         if lowtenant != lowtenant:
             print "OUCH -  Tenatns don't match"
             sys.exit(0)
